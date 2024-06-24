7b371d8853fa9bbb9fbc5fc80a343b81f1c9d8a0
Hoan Tiny <thedevil093@gmail.com>
2024-06-24 11:46:57 +0700

    test ngrok

--- src/firebase/confg.ts
+++ src/firebase/confg.ts
@@ -1,11 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { getAnalytics } from 'firebase/analytics'
 import { initializeApp } from 'firebase/app'
-import { FacebookAuthProvider, getAuth } from 'firebase/auth'
+import { getAuth, FacebookAuthProvider, connectAuthEmulator } from 'firebase/auth'
 import firebase from 'firebase/compat/app'
 
 import 'firebase/firestore'
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
 const firebaseConfig = {
   apiKey: 'AIzaSyD7JWy3XtGOmoo2pu41THao2JkbBvabmNQ',
   authDomain: 'chat-app-6a751.firebaseapp.com',
@@ -21,10 +21,13 @@ const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
 const db = getFirestore(app)
 const auth = getAuth(app)
-// connectAuthEmulator(auth, 'http://127.0.0.1:9099')
-// if (window.location.hostname === 'localhost' || window.location.hostname === 'messenger-fake.vercel.app') {
-//   connectFirestoreEmulator(db, '127.0.0.1', 8080)
-// }
+if (window.location.hostname === 'localhost') {
+  connectAuthEmulator(auth, 'http://127.0.0.1:9099')
+  connectFirestoreEmulator(db, '127.0.0.1', 8080)
+} else {
+  connectAuthEmulator(auth, 'https://a0b0-171-244-54-124.ngrok-free.app')
+  connectFirestoreEmulator(db, ' 836a-171-244-54-124.ngrok-free.app', 80)
+}
 export const fbProvider = new FacebookAuthProvider()
 // fbProvider.addScope('email')
 export { analytics, app, auth, db }
